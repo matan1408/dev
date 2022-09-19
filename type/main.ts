1ac0e93c7be8af48273b787b64de00774fb073ca
matan1408 <matans1408@gmail.com>
2022-09-19 17:16:07 +0300

    ..

---
+++ type/main.ts
@@ -0,0 +1,21 @@
+console.log('hello')
+let a =4
+let b = 8
+let c = 12
+console.log(c)
+let x = (b*3)
+console.log(b)
+console.log((a+b)/a)
+if(b>10){
+    console.log(b/2)
+}
+else{
+    console.log(b)
+}
+if(c>1 && c<10){
+    console.log("in range")
+}
+
+
+
+
