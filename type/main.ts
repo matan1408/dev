fe175a4eed2476c66886907d0e5ad832329f7054
matan1408 <matans1408@gmail.com>
2023-01-05 17:23:42 +0200

    .

--- type/main.ts
+++ type/main.ts
@@ -1,20 +1,5 @@
-console.log('hello')
-let a =4
-let b = 8
-let c = 12
-console.log(c)
-let x = (b*3)
-console.log(b)
-console.log((a+b)/a)
-if(b>10){
-    console.log(b/2)
-}
-else{
-    console.log(b)
-}
-if(c>1 && c<10){
-    console.log("in range")
-}
+let number = Math.round(math.random() * 10) + 1
+console console.log(number);
 
 
 
